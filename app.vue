c4927cdebfa35e705845a6e887ec79bc5553f3ef
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-28 21:10:33 -0400

    Add icons

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="container">
     <h1>Vue Simple Stepper</h1>
-    <a href="https://github.com/Tauromachian/vue-simple-stepper">Github Repo</a>
+
+    <div class="links-container">
+      <a href="https://github.com/Tauromachian/vue-simple-stepper">
+        <icon-helper icon="mdiGithub" width="50px" height="50px" />
+      </a>
+      <a href="https://www.npmjs.com/package/vue-simple-stepper">
+        <icon-helper icon="mdiNpm" width="50px" height="50px" />
+      </a>
+    </div>
     <app-card>
       <card-text>
         <example-one></example-one>
