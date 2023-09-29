81cdcfce3837ae6d788d60fa013bb63b85f66c4b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-28 21:34:35 -0400

    Make features section

---
+++ components/FeaturesSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Features</h2>
+  <ul>
+    <li>
+      Dynamically renders a specified number of steps based on the steps prop.
+    </li>
+    <li>Keeps track of the current step and which steps have been visited.</li>
+    <li>
+      Allows for customization of the header and action buttons via slots.
+    </li>
+    <li>Emits events when the user navigates through the steps.</li>
+    <li>
+      Handles step navigation logic, showing and hiding appropriate elements as
+      needed.
+    </li>
+  </ul>
+</template>
+
+<style>
+h2 {
+  margin-bottom: 0;
+}
+</style>
