a64dead726d276481d82ed6530d4347232d6d8e1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-30 09:24:54 -0400

    Use base section component

--- app.vue
+++ app.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="container">
-    <h1>Vue Simple Stepper</h1>
-
-    <div class="links-container">
-      <a href="https://github.com/Tauromachian/vue-simple-stepper">
-        <icon-helper icon="mdiGithub" width="50px" height="50px" />
-      </a>
-      <a href="https://www.npmjs.com/package/vue-simple-stepper">
-        <icon-helper icon="mdiNpm" width="50px" height="50px" />
-      </a>
-    </div>
-    <app-card>
-      <card-text>
-        <example-one></example-one>
-      </card-text>
-    </app-card>
-    <p>
-      Stepper component designed to guide users through a multi-step process,
-      like a registration form or setup process. It provides a clear indication
-      of the current, completed, and upcoming steps, and also allows for
-      navigating between these steps.
-    </p>
+    <base-section>
+      <h1>Vue Simple Stepper</h1>
+      <div class="links-container">
+        <a href="https://github.com/Tauromachian/vue-simple-stepper">
+          <icon-helper icon="mdiGithub" width="50px" height="50px" />
+        </a>
+        <a href="https://www.npmjs.com/package/vue-simple-stepper">
+          <icon-helper icon="mdiNpm" width="50px" height="50px" />
+        </a>
+      </div>
+      <app-card>
+        <card-text>
+          <example-one></example-one>
+        </card-text>
+      </app-card>
+      <p>
+        Stepper component designed to guide users through a multi-step process,
+        like a registration form or setup process. It provides a clear
+        indication of the current, completed, and upcoming steps, and also
+        allows for navigating between these steps.
+      </p>
+    </base-section>
     <features-section></features-section>
-    <section>
+    <base-section>
       <h2>Installation</h2>
       <p>Install it from npm</p>
       <one-line-code-highlighter>
@@ -36,77 +37,86 @@
       <one-line-code-highlighter>
         import "vue-simple-stepper/dist/style.css";
       </one-line-code-highlighter>
-    </section>
-    <div>
-      <h2>Use</h2>
-      <p>The easiest way to use is to copy and throw this in your code:</p>
-    </div>
-    <pre class="language-html" data-src="ExampleOne.vue"></pre>
-    <p>
-      Any element found in the default slot of the component will be
-      automatically added to the stepper. Notice that the steps data prop number
-      coincides with the number of elements in the stepper.
-    </p>
-    <p>
-      You can set a default starting point of the component with the step prop.
-      With it you can also control the stepper programmatically.
-    </p>
-    <p>Any time the actions of the stepper are clicked the events:</p>
-    <ul>
-      <li>click:next</li>
-      <li>click:previous</li>
-      <li>
-        click:submit Are going to be emitted in by the stepper on click of the
-        respective action.
-      </li>
-    </ul>
-    <p>
-      One common problem is the customization of the stepper to fit the
-      framework or styles of choice. In that case you can do it by using the CSS
-      variables:
-    </p>
-    <pre class="language-css" data-src="root-general-styles.css"></pre>
-    <p>
-      Or in case you want to further customize the buttons you can overwrite or
-      extend these classes:
-    </p>
-    <pre class="language-css" data-src="component-css-styles.css"></pre>
-    <p>
-      In case that you need to overwrite completely the action buttons do it by
-      using the actions slot like so:
-    </p>
-    <pre class="language-html" data-src="ExampleTwo.vue"></pre>
-    <app-card>
-      <card-text>
-        <example-two></example-two>
-      </card-text>
-    </app-card>
-    <h2>Props</h2>
-    <ul>
-      <li>
-        steps: An array that defines the steps in the stepper. The length of the
-        array determines the number of steps. Default is an empty array.
-      </li>
-      <li>
-        step: A number indicating the current step. Starts from 1 and default is
-        1.
-      </li>
-    </ul>
-    <h2>Events</h2>
-    <ul>
-      <li>click:next: Triggered when the next button is clicked.</li>
-      <li>click:previous: Triggered when the previous button is clicked.</li>
-      <li>click:submit: Triggered when the submit button is clicked.</li>
-    </ul>
-    <h2>Slots</h2>
-    <ul>
-      <li>
-        <p>
-          actions: For customizing the action buttons (next, previous, submit)
-          of the stepper.
-        </p>
-      </li>
-    </ul>
+    </base-section>
+    <base-section>
+      <div>
+        <h2>Use</h2>
+        <p>The easiest way to use is to copy and throw this in your code:</p>
+      </div>
+      <pre class="language-html" data-src="ExampleOne.vue"></pre>
+      <p>
+        Any element found in the default slot of the component will be
+        automatically added to the stepper. Notice that the steps data prop
+        number coincides with the number of elements in the stepper.
+      </p>
+      <p>
+        You can set a default starting point of the component with the step
+        prop. With it you can also control the stepper programmatically.
+      </p>
+      <p>Any time the actions of the stepper are clicked the events:</p>
+      <ul>
+        <li>click:next</li>
+        <li>click:previous</li>
+        <li>
+          click:submit Are going to be emitted in by the stepper on click of the
+          respective action.
+        </li>
+      </ul>
+      <p>
+        One common problem is the customization of the stepper to fit the
+        framework or styles of choice. In that case you can do it by using the
+        CSS variables:
+      </p>
+      <pre class="language-css" data-src="root-general-styles.css"></pre>
+      <p>
+        Or in case you want to further customize the buttons you can overwrite
+        or extend these classes:
+      </p>
+      <pre class="language-css" data-src="component-css-styles.css"></pre>
+      <p>
+        In case that you need to overwrite completely the action buttons do it
+        by using the actions slot like so:
+      </p>
+      <pre class="language-html" data-src="ExampleTwo.vue"></pre>
+      <app-card>
+        <card-text>
+          <example-two></example-two>
+        </card-text>
+      </app-card>
+    </base-section>
+    <base-section>
+      <h2>Props</h2>
+      <ul>
+        <li>
+          steps: An array that defines the steps in the stepper. The length of
+          the array determines the number of steps. Default is an empty array.
+        </li>
+        <li>
+          step: A number indicating the current step. Starts from 1 and default
+          is 1.
+        </li>
+      </ul>
+    </base-section>
+
+    <base-section>
+      <h2>Events</h2>
+      <ul>
+        <li>click:next: Triggered when the next button is clicked.</li>
+        <li>click:previous: Triggered when the previous button is clicked.</li>
+        <li>click:submit: Triggered when the submit button is clicked.</li>
+      </ul>
+    </base-section>
+    <base-section>
+      <h2>Slots</h2>
+      <ul>
+        <li>
+          <p>
+            actions: For customizing the action buttons (next, previous, submit)
+            of the stepper.
+          </p>
+        </li>
+      </ul>
+    </base-section>
   </div>
 </template>
 
@@ -129,6 +139,6 @@ import "prismjs";
 .links-container {
   display: flex;
   gap: 1em;
-  justify-content: center;
+  justify-content: left;
 }
 </style>
