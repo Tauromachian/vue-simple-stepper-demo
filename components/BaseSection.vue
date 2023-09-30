8ad41f146cc081932e24898fff48907d11e32da6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-30 11:57:17 -0400

    Add spacing

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col my-32 p-3">
+  <section class="flex flex-col my-1">
     <h2
       v-if="title"
       class="mb-3 text-3xl text-5xl self-start sm:self-center"
