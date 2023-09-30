63ee301ca7d1496566b30f4b3551f608b6ae0af3
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-30 09:21:14 -0400

    Add ExampleTwo.vue code

---
+++ components/ExampleTwo.vue
@@ -0,0 +1,54 @@
+<template>
+  <vue-simple-stepper
+    :steps="state.steps"
+    :step="state.step"
+    @click:next="nextStep"
+    @click:previous="previousStep"
+    @click:submit="submitStep"
+  >
+    <div class="content first">asdfasd</div>
+    <div class="content second">asdfasd</div>
+    <div class="content third">asdfasd</div>
+
+    <template
+      #actions="{
+        clickNext,
+        clickPrevious,
+        clickSubmit,
+        isFirstStep,
+        isLastStep,
+      }"
+    >
+      <div>
+        <button @click="clickPrevious" v-if="!isFirstStep">Previous</button>
+        <button @click="clickNext" v-if="!isLastStep">Next</button>
+        <button @click="clickSubmit" v-if="isLastStep">Submit</button>
+      </div>
+    </template>
+  </vue-simple-stepper>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+import "vue-simple-stepper/dist/style.css";
+
+import VueSimpleStepper from "vue-simple-stepper";
+
+const state = reactive({
+  steps: ["Step 1", "Step 2", "Step 3"],
+  step: 1,
+});
+
+const nextStep = () => {
+  state.step++;
+};
+
+const previousStep = () => {
+  state.step--;
+};
+
+const submitStep = () => {
+  console.log("Step submitted");
+};
+</script>
