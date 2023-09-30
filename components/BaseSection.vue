ae1e121e8e125e38af966374b61cc758ca8e76fc
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2023-09-30 09:21:32 -0400

    Make base section component

---
+++ components/BaseSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="flex flex-col my-32 p-3">
+    <h2
+      v-if="title"
+      class="mb-3 text-3xl text-5xl self-start sm:self-center"
+      :class="titleAlignment"
+    >
+      {{ title }}
+    </h2>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BaseSection",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    titleAlignment: {
+      type: [String],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
